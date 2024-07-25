7105dfc952af364756fdd769a454db4b57099da5
ayushkhanvilkar10 <khanvilkar.a@northeastern.edu>
2024-07-25 17:03:01 -0400

    Pages folder with TeamPage component created and called from App component

---
+++ the_governance_lab_teams_page/src/pages/TeamPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TeamPage: React.FC = () => {
+  return (
+    <div>
+      <main>
+        This is the Team Page. All the Team Cards will go here.
+      </main>
+    </div>
+  );
+};
+
+export default TeamPage;
