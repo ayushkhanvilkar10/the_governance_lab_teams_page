63b7994216bc366eb1f37c86d98ae1cd668ee6ec
ayushkhanvilkar10 <khanvilkar.a@northeastern.edu>
2024-07-25 16:56:59 -0400

    vite setup completed and components folder created

---
+++ the_governance_lab_teams_page/src/components/Navigation.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navigation: React.FC = () => {
+  return (
+    <nav>
+      <h1>Team Page</h1>
+      {/* Add navigation links here */}
+    </nav>
+  );
+};
+
+export default Navigation;
