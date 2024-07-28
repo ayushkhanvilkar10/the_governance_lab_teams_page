b12d8cc259de28264686dee0f5d76a91ca14b7dc
ayushkhanvilkar10 <khanvilkar.a@northeastern.edu>
2024-07-28 18:46:24 -0400

    TeamCard styling

--- the_governance_lab_teams_page/src/components/Footer/Footer.tsx
+++ the_governance_lab_teams_page/src/components/Footer/Footer.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Footer.css'; // Ensure this line is added to import the CSS file
+import footerImage from '../../assets/Footer_Image.png'; // Import the footer image
 
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
+      <img src={footerImage} alt="Footer Logo" className="footer-image" />
       <p>© 2024 Governance Lab</p>
     </footer>
   );
