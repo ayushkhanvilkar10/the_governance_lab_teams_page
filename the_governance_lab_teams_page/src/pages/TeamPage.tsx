8f76d20ae3dc82d4b01319645ba17a9565c93c67
ayushkhanvilkar10 <khanvilkar.a@northeastern.edu>
2024-07-28 00:04:06 -0400

    Fixed parsing issue with breaks

--- the_governance_lab_teams_page/src/pages/TeamPage.tsx
+++ the_governance_lab_teams_page/src/pages/TeamPage.tsx
@@ -31,7 +31,7 @@ const TeamPage: React.FC = () => {
   if (loading) return (
     <div className="spinner-container">
       <Spinner animation="border" role="status">
-        <span className="sr-only"></span>
+        <span className="sr-only">Loading...</span>
       </Spinner>
     </div>
   );
@@ -56,13 +56,18 @@ const TeamPage: React.FC = () => {
     }));
   };
 
+  const preprocessBio = (bio: string): string => {
+    // Replace \n</br></br> with a single <br> tag
+    return bio.replace(/\n<\/br><\/br>/g, '<br></br>');
+  };
+
   const sortedTeamMembers = teamMembersArray.map(selected_member => {
     const member = teamMembers.find(member => member.name === selected_member.name && member.bio_short);
     return member ? {
       name: member.name,
       title: member.title,
       bio_short: member.bio_short,
-      bio: member.bio,
+      bio: member.bio ? preprocessBio(member.bio) : '', // Preprocess bio to replace sequences
       picture_blog2020: member.picture_blog2020,
       picture: member.picture,
       projects: member.projects,
@@ -78,10 +83,11 @@ const TeamPage: React.FC = () => {
             const pictureUrl = member.picture_blog2020 || (member.picture ? `https://content.thegovlab.com/assets/${member.picture.id}` : '');
             const isExpanded = expandedMembers[member.name];
             const areProjectsVisible = projectsVisible[member.name];
+            const showReadMoreButton = member.bio !== member.bio_short;
 
             return (
-              <>
-                <Card key={index} className="mb-3 custom-card">
+              <React.Fragment key={index}>
+                <Card className="mb-3 custom-card">
                   <Row noGutters>
                     <Col xs={12} md={3}>
                       {pictureUrl && (
@@ -101,13 +107,15 @@ const TeamPage: React.FC = () => {
                         {member.bio && isExpanded && <Card.Text className='card-text-custom'>{parse(member.bio)}</Card.Text>}
                         {member.bio !== null && member.bio !== "NULL" && (
                           <div className='button-container'>
-                            <Button className='read-more' variant="link" onClick={() => handleToggle(member.name,member.url)}>
-                              {isExpanded ? 'LESS' : 'MORE'}
-                              <i className='material-icons'>{isExpanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'}</i>
-                            </Button>
+                            {showReadMoreButton && (
+                              <Button className='read-more' variant="link" onClick={() => handleToggle(member.name, member.url)}>
+                                {isExpanded ? 'LESS' : 'MORE'}
+                                <i className='material-icons'>{isExpanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'}</i>
+                              </Button>
+                            )}
                             {member.projects && member.projects.length > 0 && (
                               <Button className='read-more' variant="link" onClick={() => handleProjectsToggle(member.name)}>
-                                 PROJECTS
+                                PROJECTS
                                 <i className='material-icons'>{areProjectsVisible ? 'keyboard_arrow_up' : 'keyboard_arrow_down'}</i>
                               </Button>
                             )}
@@ -128,8 +136,8 @@ const TeamPage: React.FC = () => {
                     </Col>
                   </Row>
                 </Card>
-               <hr className="full-width-divider"></hr>
-              </>
+                <hr className="full-width-divider" />
+              </React.Fragment>
             );
           })}
         </main>
