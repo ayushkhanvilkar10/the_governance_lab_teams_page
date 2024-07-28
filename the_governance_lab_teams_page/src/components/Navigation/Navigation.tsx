8303474417c26144b2da51138a46a50debd5337f
ayushkhanvilkar10 <khanvilkar.a@northeastern.edu>
2024-07-28 16:14:49 -0400

    Navigation bar with icons added

--- the_governance_lab_teams_page/src/components/Navigation/Navigation.tsx
+++ the_governance_lab_teams_page/src/components/Navigation/Navigation.tsx
@@ -6,9 +6,15 @@ import logo from '../../assets/the-govlab-logo-white.png';
 const Navigation: React.FC = () => {
   return (
     <Navbar className="custom-navbar" expand="lg">
-      <Container className="justify-content-center">
-        <Navbar.Brand href="#home" className="mx-auto">
-          <img src={logo} alt="The GovLab Logo" className="govlab-logo" />
+      <Container fluid className="navbar-container">
+        <div className='icons-container'>
+          <i className='material-icons'>menu</i>
+          <i className='material-icons'>search</i>
+        </div>
+        <Navbar.Brand href="#home" className="mx-auto logo-container">
+          <a href='https://thegovlab.org/'>
+            <img src={logo} alt="The GovLab Logo" className="govlab-logo" />
+          </a>
         </Navbar.Brand>
       </Container>
     </Navbar>
